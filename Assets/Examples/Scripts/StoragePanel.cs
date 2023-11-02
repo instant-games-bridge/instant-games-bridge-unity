d3e84e8b6bce5c9787fccf9f43a4ad1b4c3abea7
Mewton Games <mewton.games@gmail.com>
2023-11-02 10:13:34 +0200

    Fixed storage examples

--- Assets/Examples/Scripts/StoragePanel.cs
+++ Assets/Examples/Scripts/StoragePanel.cs
@@ -9,29 +9,18 @@ namespace Examples
     public class StoragePanel : MonoBehaviour
     {
         [SerializeField] private Text _defaultTypeText;
-
         [SerializeField] private Text _isLocalStorageSupportedText;
-        
         [SerializeField] private Text _isLocalStorageAvailableText;
-
         [SerializeField] private Text _isPlatformInternalSupportedText;
-        
         [SerializeField] private Text _isPlatformInternalAvailableText;
-
         [SerializeField] private InputField _coinsInput;
-
         [SerializeField] private InputField _levelInput;
-
         [SerializeField] private Button _setStorageDataButton;
-
         [SerializeField] private Button _getStorageDataButton;
-
         [SerializeField] private Button _deleteStorageDataButton;
-
         [SerializeField] private GameObject _overlay;
 
         private const string _coinsKey = "coins";
-
         private const string _levelKey = "level";
 
         private void Start()
@@ -53,72 +42,34 @@ namespace Examples
 
             int.TryParse(_coinsInput.text, out var coins);
             var level = _levelInput.text;
-
-            Bridge.storage.Set(_coinsKey, coins, null);
-            Bridge.storage.Set(_levelKey, level);
-
-            var keys = new List<string> { "example_1", "example_2", "example_3" };
-            var values = new List<object> { 1, "test", true };
-            Bridge.storage.Set(keys, values, success => _overlay.SetActive(false));
+            
+            Bridge.storage.Set(new List<string> { _coinsKey, _levelKey }, new List<object> { coins, level }, _ => _overlay.SetActive(false));
         }
 
         private void OnGetStorageDataButtonClicked()
         {
             _overlay.SetActive(true);
 
-            Bridge.storage.Get(_coinsKey, (success, data) =>
+            var keys = new List<string> { _coinsKey, _levelKey };
+            Bridge.storage.Get(keys, (success, data) =>
             {
-                if (data != null)
+                if (data[0] != null)
                 {
-                    int.TryParse(data, out var coins);
+                    int.TryParse(data[0], out var coins);
                     _coinsInput.text = coins.ToString();
                 }
                 else
                 {
                     _coinsInput.text = "0";
                 }
-            });
 
-            Bridge.storage.Get(_levelKey, (success, data) =>
-            {
-                if (data != null)
+                if (data[1] != null)
                 {
-                    _levelInput.text = data;
+                    _levelInput.text = data[1];
                 }
                 else
                 {
                     _levelInput.text = "default_level";
-                }
-            });
-
-            var keys = new List<string> { "example_1", "example_2", "example_3" };
-            Bridge.storage.Get(keys, (success, data) =>
-            {
-                if (data[0] != null)
-                {
-                    Debug.Log($"example_1: {data[0]}");
-                }
-                else
-                {
-                    Debug.Log("example_1: default_value");
-                }
-
-                if (data[1] != null)
-                {
-                    Debug.Log($"example_2: {data[1]}");
-                }
-                else
-                {
-                    Debug.Log("example_2: default_value");
-                }
-
-                if (data[2] != null)
-                {
-                    Debug.Log($"example_3: {data[2]}");
-                }
-                else
-                {
-                    Debug.Log("example_3: default_value");
                 }
 
                 _overlay.SetActive(false);
@@ -129,7 +80,7 @@ namespace Examples
         {
             _overlay.SetActive(true);
 
-            var keys = new List<string> { _coinsKey, _levelKey, "example_1", "example_2", "example_3" };
+            var keys = new List<string> { _coinsKey, _levelKey };
             Bridge.storage.Delete(keys, success =>
             {
                 _coinsInput.text = string.Empty;
